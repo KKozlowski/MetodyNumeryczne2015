b2277b8eaaf2aa581cdc6eb924703a94c749ca59
kkozlowski <septuater@gmail.com>
2015-03-11 09:19:14 +0100

    Dodatkowy wykres poglądowy, wczytywanie eps.

--- NUM1/main.cpp
+++ NUM1/main.cpp
@@ -27,8 +27,6 @@ template<class F>void bisekcja(double start, double end, F funk, double epsilon)
         double center = (start+end)/2;
         double centerValue = funk(center);
         if(abs(centerValue) < epsilon){
-            //rozwiazania.push_back(center);
-            //cout << center << endl;
             miejZerBi = center;
             foundMiejZerBi = true;
         } else {
@@ -47,17 +45,39 @@ template<class F> double siecznych(F funk, double a, double b, double eps){
     x0 = a - funk(a) * (a - b) / (funk(a) - funk(b));
     if (x0 != x0){cout << "blad sieczna pionowa, najblizsza wartosc " << x00 << endl; return 0;}
     if (fabs(funk(x0)) < eps){
-        //cout << x0 << endl;
-        //cout << funk(x0) << endl;
         foundMiejZerSiecz = true;
         return x0;
     }
     else{
-        //cout << x0 << endl;
-        //cout << funk(x0) << endl;
         return siecznych(funk, x0, a, eps);
     }
     x00=x0;
+}
+
+void drawPreview(){
+    Gnuplot *preview;
+    preview->set_title( "Wykres" );
+    preview->set_xlabel( "X" );
+    preview->set_ylabel( "Y" );
+
+    preview->set_style( "lines" );
+    preview->set_grid();
+
+    // zakres osi x
+    preview->set_xrange( -5 , 5 ) ;
+    int precyzja = 50;
+    double end=5;
+    double start=-5;
+    double skok = (end-start)/50;
+    vector<double> x;
+    vector<double> y;
+    for(double d=start; d<=end;d+=skok){
+        x.push_back(d);
+        y.push_back(sinus(d));
+    }
+
+    preview->plot_xy( x, y, "Wykres funkcji" );
+
 }
 
 int main()
@@ -77,38 +97,11 @@ int main()
         funk=[](double x) -> double {return power(2,x);};
         break;
     case 3:
-        funk=[](double x) -> double {return wielomian(2,3,2,1,x);};
+        funk=[](double x) -> double {return wielomian(2,3,2,1.05,x);};
         break;
     default:
         return 0;
     }
-
-    double start, end;
-    double eps = 0.001;
-    cout << "Podaj poczatek zakresu: ";
-    cin >> start;
-    cout << "Podaj koniec zakresu: ";
-    cin >> end;
-
-
-
-    cout << "METODA BISEKCJI:\n";
-    bisekcja(start,end,funk,0.001);
-    miejZerYBi=funk(miejZerBi);
-    if(foundMiejZerBi)
-        cout << "Znalezione miejsce zerowe: (" << miejZerBi << ", " << miejZerYBi << ")";
-    else
-        cout << "Nie znaleziono odpowiedniego miejsca zerowego";
-
-    cout << endl << endl;
-
-    cout << "METODA SIECZNYCH:\n";
-    miejZerSiecz = siecznych(funk, start, end, 0.001);
-    miejZerYSiecz=funk(miejZerSiecz);
-    if(foundMiejZerBi)
-        cout << "Znalezione miejsce zerowe: (" << miejZerSiecz << ", " << miejZerYSiecz << ")";
-    else
-        cout << "Nie znaleziono odpowiedniego miejsca zerowego";
 
     Gnuplot main_plot;
 
@@ -119,33 +112,84 @@ int main()
     main_plot.set_style( "lines" );
     main_plot.set_grid();
 
-    // zakres osi x
-    main_plot.set_xrange( start , end ) ;
+    {
+        double start = -5;
+        double end = 5;
+        main_plot.set_xrange( start , end ) ;
+        int precyzja = 50;
+        double skok = (end-start)/50;
+        vector<double> x;
+        vector<double> y;
+        for(double d=start; d<=end;d+=skok){
+            x.push_back(d);
+            y.push_back(funk(d));
+        }
 
-    int precyzja = 50;
-    double skok = (end-start)/50;
-    vector<double> x;
-    vector<double> y;
-    for(double d=start; d<=end;d+=skok){
-        x.push_back(d);
-        y.push_back(funk(d));
+        main_plot.plot_xy( x, y, "Wykres pogladowy." );
     }
 
-    main_plot.plot_xy( x, y, "Wykres funkcji" );
+    double start, end;
+    double eps = 0.001;
+    cout << "Podaj poczatek zakresu: ";
+    cin >> start;
+    cout << "Podaj koniec zakresu: ";
+    cin >> end;
 
-    main_plot.set_style( "points" );
-    main_plot.set_pointsize( 2.0 );
+    while(eps>=0){
+        cout << "\nPodaj wymagana dokladnosc (ujemna zakonczy program): ";
+        cin >> eps;
+        if(eps<0) break;
+
+        //drawPreview();
+
+        cout << "METODA BISEKCJI:\n";
+        bisekcja(start,end,funk,eps);
+        miejZerYBi=funk(miejZerBi);
+        if(foundMiejZerBi)
+            cout << "Znalezione miejsce zerowe: (" << miejZerBi << ", " << miejZerYBi << ")";
+        else
+            cout << "Nie znaleziono odpowiedniego miejsca zerowego";
+
+        cout << endl << endl;
+
+        cout << "METODA SIECZNYCH:\n";
+        miejZerSiecz = siecznych(funk, start, end, eps);
+        miejZerYSiecz=funk(miejZerSiecz);
+        if(foundMiejZerBi)
+            cout << "Znalezione miejsce zerowe: (" << miejZerSiecz << ", " << miejZerYSiecz << ")";
+        else
+            cout << "Nie znaleziono odpowiedniego miejsca zerowego";
+
+        main_plot.reset_plot();
+
+        // zakres osi x
+        main_plot.set_xrange( start , end ) ;
+
+        int precyzja = 50;
+        double skok = (end-start)/50;
+        vector<double> x;
+        vector<double> y;
+        for(double d=start; d<=end;d+=skok){
+            x.push_back(d);
+            y.push_back(funk(d));
+        }
+        main_plot.set_style( "lines" );
+        main_plot.plot_xy( x, y, "Wykres funkcji" );
+
+        main_plot.set_style( "points" );
+        main_plot.set_pointsize( 2.0 );
 
 
-    vector<double> miejZerX;
-    miejZerX.push_back(miejZerBi);
-    miejZerX.push_back(miejZerSiecz);
-    vector<double> miejZerY;
-    miejZerY.push_back(miejZerYBi);
-    miejZerY.push_back(miejZerYSiecz);
-    cout << foundMiejZerBi <<  foundMiejZerSiecz;
-    if(foundMiejZerBi && foundMiejZerSiecz)
-        main_plot.plot_xy( miejZerX, miejZerY, "Znalezione miejsca zerowe" );
+        vector<double> miejZerX;
+        miejZerX.push_back(miejZerBi);
+        miejZerX.push_back(miejZerSiecz);
+        vector<double> miejZerY;
+        miejZerY.push_back(miejZerYBi);
+        miejZerY.push_back(miejZerYSiecz);
+        //cout << foundMiejZerBi <<  foundMiejZerSiecz;
+        if(foundMiejZerBi && foundMiejZerSiecz)
+            main_plot.plot_xy( miejZerX, miejZerY, "Znalezione miejsca zerowe" );
+    }
 
     getchar();
     getchar();
