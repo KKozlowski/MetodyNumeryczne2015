9b87a23082716f6ea7efd40a1755bced7dfc2914
natakato <natakaro@gmail.com>
2015-03-31 16:26:49 +0200

    Dodana funkcja wczytywania pliku

--- NUM2/main.cpp
+++ NUM2/main.cpp
@@ -3,12 +3,48 @@
 #include "matrix.h"
 #include "equation.h"
 #include "wektor.h"
+#include <fstream>
 using namespace std;
+
+
+Matrix readfile(string filename){
+    //string filename = "1";
+    ifstream plik;
+    string txt = ".txt";
+
+    plik.open(filename.c_str()+txt);
+    if(!plik.good()){cout << "blad" << endl; return false;}
+
+    int size;
+    plik >> size;
+    Matrix x(size);
+
+    //wczytywanie macierzy
+    for(int i=1; i<=size; i++){
+        for (int j=1; j<=size+1; j++){
+            plik  >> x[i][j];
+        }
+    }
+
+    //wypisywanie macierzy
+    /*
+    for(int i=1; i<=size; i++){
+        for (int j=1; j<=size+1; j++){
+            cout << x[i][j] << " ";
+        }
+      cout << endl;
+    }
+    */
+
+    return x;
+}
 
 int main()
 {
+    string filename;
+    cin >> filename;
     int ileRownan = 3;
-    Matrix m(3);
+    Matrix m = readfile(filename);
     /*
     m[1][1] =3;
     m[1][2] =3;
@@ -25,7 +61,7 @@ int main()
     m[3][3] =1;
     m[3][4] =8;
     */
-
+    /*
     m[1][1] =2;
     m[1][2] =3;
     m[1][3] =-1;
@@ -40,6 +76,7 @@ int main()
     m[3][2] =2;
     m[3][3] =-5;
     m[3][4] =3;
+    */
 
     m.Transform();
     m.Write();
