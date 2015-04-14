31462e995e8c07cad762d1ebf320024226414305
kkozlowski <septuater@gmail.com>
2015-04-14 18:03:59 +0200

    Porządek w kodzie zadania 3, dodano możliwość wyboru pliku z węzłami i wartościami funkcji, dodano sprawka z drugiego i trzeciego zadania.

--- NUM3/main.cpp
+++ NUM3/main.cpp
@@ -12,8 +12,9 @@ using namespace std;
 vector<double> wezly;
 vector<double> wartosci;
 vector<double> wspol;
+bool wartosciZPliku = false;
 
-vector<double> readfile(string filename){
+vector<double> czytaj(string filename){
     ifstream plik;
     string txt = ".txt";
 
@@ -43,19 +44,18 @@ vector<double> obliczWezly(double (*funk)(double), vector<double> w){
     return result;
 }
 
-double delta(int i, int k){
+double iloraz(int i, int k){
     if(k == 0)
         return (wartosci[i]);
     else
-        return (delta(i+1,k-1) - delta(i,k-1))/(wezly[i+k]-wezly[i]);
+        return (iloraz(i+1,k-1) - iloraz(i,k-1))/(wezly[i+k]-wezly[i]);
 }
 
 
 void obliczWsp(){
     wspol.clear();
-    //wspol.push_back( wartosci[0]);
     for(int i=0; i<wartosci.size(); i++){
-        wspol.push_back( delta(0,i));
+        wspol.push_back( iloraz(0,i));
     }
 }
 
@@ -77,14 +77,14 @@ void wypiszWektor(vector<double> w, string opis){
     wypiszWektor(w);
 }
 
-double iloraz(int x1, int x2){
+/*double iloraz(int x1, int x2){
     if (x1 == x2) return wartosci[x1];
 
     double topOne = iloraz(x1+1,x2);
     double topTwo = iloraz(x1,x2-1);
-    double mianownik = wartosci[x2] - wartosci[x1];
+    double mianownik = wezly[x2] - wezly[x1];
     return (topOne-topTwo)/mianownik;
-}
+}*/
 
 double interpol(double x){
     double y = 0;
@@ -120,47 +120,63 @@ int main()
 {
     Gnuplot::set_GNUPlotPath( GNUPLOT_PATH );
 
-    wezly = readfile("one");
+    cout << "Podaj nazwe pliku z lista wezlow: ";
+    string nazwaPliku;
+    cin >> nazwaPliku;
+    wezly = czytaj(nazwaPliku);
     wypiszWektor(wezly, "Wezly");
+
+    cout << "Podaj nazwe pliku z lista wartosci \n(wpisz '0' aby wybrac gotowa funkcje): ";
+    cin >> nazwaPliku;
+    if (nazwaPliku!="0"){
+        wartosci = czytaj(nazwaPliku);
+        if (wartosci.size() != wezly.size())
+            cout << "Niezgodna liczba argumentow i wartosci.";
+        else
+            wartosciZPliku = true;
+    }
 
     cout << endl;
 
-    cout << "Wybierz funkcje: \n1. Sinus\n2. Wykladnicza\n3. Wielomian \n4. Wielomian przykladowy \n> ";
-    int wybor;
     double (*funk)(double);
-    cin >> wybor;
+    if(!wartosciZPliku){
+        cout << "Wybierz funkcje: \n1. Sinus\n2. Wykladnicza\n3. Wielomian \n4. Wielomian przykladowy \n> ";
+        int wybor;
 
-    switch(wybor){
-    case 1:
-        funk=sinus;
-        break;
-    case 2:
-        funk=[](double x) -> double {return power(2,x);};
-        break;
-    case 3:
-        cout<<"Podaj stopien wielomianu: ";
-        cin>>stopien;
-        wspolczynniki = new double [stopien+1];
-        //wczytanie wspolczynnikow
-        for(int i=0;i<=stopien;i++)
-        {
-          cout<<"Podaj wspolczynnik stojacy przy potedze "<<stopien-i<<": ";
-          cin>>wspolczynniki[i];
+        cin >> wybor;
+
+        switch(wybor){
+        case 1:
+            funk=sinus;
+            break;
+        case 2:
+            funk=[](double x) -> double {return power(2,x);};
+            break;
+        case 3:
+            cout<<"Podaj stopien wielomianu: ";
+            cin>>stopien;
+            wspolczynniki = new double [stopien+1];
+            //wczytanie wspolczynnikow
+            for(int i=0;i<=stopien;i++)
+            {
+              cout<<"Podaj wspolczynnik stojacy przy potedze "<<stopien-i<<": ";
+              cin>>wspolczynniki[i];
+            }
+            funk=[](double x) -> double {return horner(wspolczynniki,stopien,x);};
+            break;
+        case 4:
+            funk=[](double x) -> double {return wielomianprosty(2,3,2,1.05,x);};
+            break;
+        default:
+            return 0;
         }
-        funk=[](double x) -> double {return horner(wspolczynniki,stopien,x);};
-        break;
-    case 4:
-        funk=[](double x) -> double {return wielomianprosty(2,3,2,1.05,x);};
-        break;
-    default:
-        return 0;
-    }
 
-    wartosci = obliczWezly(funk, wezly);
+        wartosci = obliczWezly(funk, wezly);
+    }
     wypiszWektor(wartosci, "Wartosci funkcji w wezlach");
     obliczWsp();
     cout << "Liczba wspol: " << wspol.size() << endl;
-    wypiszWektor(wspol, "Wspolczynniki a: ");
+    wypiszWektor(wspol, "Wspolczynniki a");
 
     Gnuplot main_plot;
 
@@ -168,16 +184,24 @@ int main()
     main_plot.set_xlabel( "X" );
     main_plot.set_ylabel( "Y" );
 
-    main_plot.set_style( "lines" );
+
     main_plot.set_grid();
 
+    double start = -5;
+    double end = 5;
+    main_plot.set_style( "points" );
+    main_plot.set_pointsize( 2.0 );
+    main_plot.set_xrange( start , end ) ;
+    main_plot.set_yrange(-2,2);
+
     {
-        double start = -5;
-        double end = 5;
-        main_plot.set_xrange( start , end ) ;
-        main_plot.set_yrange(-2,2);
-        int precyzja = 100;
-        double skok = (end-start)/50;
+        main_plot.plot_xy( wezly, wartosci, "Wezly." );
+    }
+    main_plot.set_style( "lines" );
+    if(!wartosciZPliku)
+    {
+        int precyzja = 10000;
+        double skok = (end-start)/precyzja;
         vector<double> x;
         vector<double> y;
         for(double d=start; d<=end;d+=skok){
@@ -185,15 +209,11 @@ int main()
             y.push_back(funk(d));
         }
 
-        main_plot.plot_xy( x, y, "Wykres pogladowy." );
+        main_plot.plot_xy( x, y, "Wykres funkcji." );
     }
     {
-        double start = -5;
-        double end = 5;
-        main_plot.set_xrange( start , end ) ;
-        main_plot.set_yrange(-2,2);
-        int precyzja = 50;
-        double skok = (end-start)/10000;
+        int precyzja = 10000;
+        double skok = (end-start)/precyzja;
         vector<double> x;
         vector<double> y;
         for(double d=start; d<=end;d+=skok){
@@ -202,7 +222,7 @@ int main()
             y.push_back(interpol(d));
         }
 
-        main_plot.plot_xy( x, y, "Wykres interpolacyjne." );
+        main_plot.plot_xy( x, y, "Wykres interpolacyjny." );
     }
 
 
